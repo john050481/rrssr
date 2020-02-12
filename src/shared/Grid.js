46a2169793a15e20bdcea9bcca8e427241de3368
john050481 <john0504@ya.ru>
2020-02-12 13:46:51 +0300

    add Hooks in Grid

--- src/shared/Grid.js
+++ src/shared/Grid.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import {
@@ -6,47 +6,36 @@ import {
   fetchReposIfNeeded
 } from '.././store/actions'
 
-class Grid extends Component {
-  constructor(props) {
-    super(props)
+function Grid (props) {
+  const { repos, isFetching } = props;
 
-    this.fetchRepos = this.fetchRepos.bind(this)
-  }
-  componentDidMount () {
-    if (!this.props.repos.length && !this.props.isFetching) {
-      this.fetchRepos(this.props.match.params.id)
-    }
-  }
-  componentDidUpdate (prevProps, prevState) {
-    if (prevProps.match.params.id !== this.props.match.params.id) {
-      this.fetchRepos(this.props.match.params.id)
-    }
-  }
-  fetchRepos (lang) {
-    this.props.selectLanguage(lang);
-    this.props.fetchReposIfNeeded(lang);
-  }
-  render() {
-    const { repos, isFetching } = this.props;
+  useEffect(() => {
+    props.selectLanguage(props.match.params.id);
+    //return () => { /*componentWillUnmount*/ };
+  }, [props.match.params.id]);
 
-    if (isFetching === true) {
-      return <p>LOADING</p>
-    }
+  useEffect(() => {
+    if (!props.isFetching && props.selectedLanguage) props.fetchReposIfNeeded(props.selectedLanguage);
+    //return () => { /*componentWillUnmount*/ };
+  }, [props.selectedLanguage]);
 
-    return (
-      <ul style={{display: 'flex', flexWrap: 'wrap'}}>
-        {repos.map(({ name, owner, stargazers_count, html_url }) => (
-          <li key={name} style={{margin: 30}}>
-            <ul>
-              <li><a href={html_url}>{name}</a></li>
-              <li>@{owner.login}</li>
-              <li>{stargazers_count} stars</li>
-            </ul>
-          </li>
-        ))}
-      </ul>
-    )
+  if (isFetching === true) {
+    return <p>LOADING</p>
   }
+
+  return (
+    <ul style={{display: 'flex', flexWrap: 'wrap'}}>
+      {repos.map(({ name, owner, stargazers_count, html_url }) => (
+        <li key={name} style={{margin: 30}}>
+          <ul>
+            <li><a href={html_url}>{name}</a></li>
+            <li>@{owner.login}</li>
+            <li>{stargazers_count} stars</li>
+          </ul>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 function mapStateToProps(state) {
@@ -55,7 +44,7 @@ function mapStateToProps(state) {
       selectedLanguage
       ] || {
     isFetching: false,
-    items: []
+    repos: []
   }
   return {
     selectedLanguage,
